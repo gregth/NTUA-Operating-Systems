65dae3f0089b0cf882e4cf1208396a0a39201418
Grigoris Thanasoulas <gregthanasoulas@gmail.com>
2017-12-18 10:46:53 +0200

    Add solution for problem 2 / Assignment 3

--- Assignment 3/mandel.c
+++ Assignment 3/mandel.c
@@ -11,10 +11,16 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
 
 #include "mandel-lib.h"
 
 #define MANDEL_MAX_ITERATION 100000
+
+// Declare a pointer for semaphore in global context
+sem_t* semaphore;
+int N_THREADS;
 
 /***************************
  * Compile-time parameters *
@@ -32,7 +38,7 @@ int x_chars = 90;
 */
 double xmin = -1.8, xmax = 1.0;
 double ymin = -1.0, ymax = 1.0;
-	
+
 /*
  * Every character in the final output is
  * xstep x ystep units wide on the complex plane.
@@ -78,7 +84,7 @@ void compute_mandel_line(int line, int color_val[])
 void output_mandel_line(int fd, int color_val[])
 {
 	int i;
-	
+
 	char point ='@';
 	char newline='\n';
 
@@ -98,6 +104,19 @@ void output_mandel_line(int fd, int color_val[])
 	}
 }
 
+void *safe_malloc(size_t size)
+{
+	void *p;
+
+	if ((p = malloc(size)) == NULL) {
+		fprintf(stderr, "Out of memory, failed to allocate %zd bytes\n",
+			size);
+		exit(1);
+	}
+
+	return p;
+}
+
 void compute_and_output_mandel_line(int fd, int line)
 {
 	/*
@@ -106,23 +125,71 @@ void compute_and_output_mandel_line(int fd, int line)
 	int color_val[x_chars];
 
 	compute_mandel_line(line, color_val);
+    // Wait for the corresponding sempahore, then output line
+    sem_wait(&semaphore[line % N_THREADS]);
 	output_mandel_line(fd, color_val);
+    // Increase sempahore of next line
+    sem_post(&semaphore[(line + 1) % N_THREADS]);
 }
 
-int main(void)
+
+
+void *thread_function(void* line_arg) {
+    int* line_ptr = (int*)line_arg;
+    // Compute and  lines line, line  + N_THREADS, line + 2*NTHREADS, etc
+    int line;
+    for (line = *line_ptr; line < y_chars; line += N_THREADS) {
+        compute_and_output_mandel_line(1, line);
+    }
+    pthread_exit(0);
+}
+
+int main(int argc, char* argv[])
 {
+    // Check if sufficient arguments have been provided
+    if (argc != 2) {
+        printf("Usage: ./mandel <N_THREADS>\n");
+        exit(1);
+    }
+
+    N_THREADS = atoi(argv[1]);
+    printf("Running for N_THREADS = %d\n", N_THREADS);
+
+    /* Allocate memory space for semaphores, initialize
+     * them to 0, excpet for the semaphore[0] wich shoulde
+     * be initialized to value 1
+     */
+    semaphore = safe_malloc(N_THREADS * sizeof(sem_t));
+    sem_init(&semaphore[0], 0, 1);
+    int i = 0;
+    for (i = 1; i < N_THREADS; i++) {
+        sem_init(&semaphore[i], 0, 0);
+    }
+
 	int line;
 
 	xstep = (xmax - xmin) / x_chars;
 	ystep = (ymax - ymin) / y_chars;
 
+    // Allocate space for threads type
+    pthread_t *threads = safe_malloc(N_THREADS * sizeof(pthread_t));
+    int* args = safe_malloc(N_THREADS * sizeof(int));
+
+
 	/*
 	 * draw the Mandelbrot Set, one line at a time.
 	 * Output is sent to file descriptor '1', i.e., standard output.
 	 */
-	for (line = 0; line < y_chars; line++) {
-		compute_and_output_mandel_line(1, line);
+
+    // Create N_THREADS
+	for (line = 0; line < N_THREADS; line++) {
+        args[line] = line;
+        pthread_create(&threads[line], NULL, thread_function, &args[line]);
 	}
+
+	for (line = 0; line < N_THREADS; line++) {
+        pthread_join(threads[line], NULL);
+    }
 
 	reset_xterm_color(1);
 	return 0;
